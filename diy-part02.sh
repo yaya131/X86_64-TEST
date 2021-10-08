3d6df0f5f7c6e4a40b3a2bf9463408ef6505dade
jingleijack <tb6750217@gmail.com>
2021-10-08 18:09:42 +0800

    Update diy-part02.sh

--- diy-part02.sh
+++ diy-part02.sh
@@ -33,11 +33,19 @@ git clone https://github.com/kiddin9/openwrt-packages.git package/openwrt-packag
 
 #添加额外非必须软件包
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+
+git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
+
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
 git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
+
 git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
+
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+
 #添加smartdns
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
