28730b15b26f87d084b8c7d5fa03f44b83edeeec
jingleijack <tb6750217@gmail.com>
2021-10-08 17:55:42 +0800

    Update diy-part01.sh

--- diy-part01.sh
+++ diy-part01.sh
@@ -6,7 +6,7 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
+# File name: diy-part01.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
@@ -14,12 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-
-#echo 'src-git packages https://git.openwrt.org/feed/packages.git;openwrt-21.02
-#echo 'src-git luci https://git.openwrt.org/project/luci.git;openwrt-21.02
-#echo 'src-git routing https://git.openwrt.org/feed/routing.git;openwrt-21.02
-#echo 'src-git telephony https://git.openwrt.org/feed/telephony.git;openwrt-21.02
-
-echo 'src-git openwrt-packages https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+#echo 'src-git openwrt-packages https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
