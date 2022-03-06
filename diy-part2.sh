4c30df97fd0e2679f57ef314f0444bf5c27a35f6
jingleijack <tb6750217@gmail.com>
2022-03-06 21:43:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -35,6 +35,8 @@ git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/l
 # Add luci-theme-argon
 git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
 
+git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
+
 #git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 
 #添加额外非必须软件包
