747144c900cabc053d7268fa0a8dc9f5cf4f70ab
yaya <tb6750217@gmail.com>
2022-10-15 22:08:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,7 +18,7 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 
 
 rm -rf package/lean/autosamba && git clone https://github.com/sirpdboy/autosamba.git package/autosamba
-rm -rf package/lean/automount && git clone https://github.com/sirpdboy/automount.git package/automount
+
 rm -rf package/lean/autocore && git clone https://github.com/sirpdboy/myautocore.git package/myautocore
 
 git clone https://github.com/kenzok8/small-package.git package/small-package
