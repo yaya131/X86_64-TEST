8b720a07ca49ef7dda0ae6f613799e04d4c491a3
yaya <tb6750217@gmail.com>
2023-07-07 13:30:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,8 +22,6 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 #rm -rf package/lean/autocore && git clone https://github.com/sirpdboy/myautocore.git package/myautocore
 
 git clone https://github.com/kenzok8/small-package.git package/small-package
-
-
 git clone https://github.com/linkease/istore.git package/istore
 #sirpdboy
 #git clone https://github.com/kiddin9/openwrt-packages.git package/openwrt-packages
@@ -38,24 +36,10 @@ git clone https://github.com/linkease/istore.git package/istore
 #修正连接数（by ベ七秒鱼ベ）
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
-# themes添加（svn co 命令意思：指定版本如https://github）
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
-git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
-# Add luci-theme-argon
-git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
-
-#git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
-
-#git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-
 #添加额外非必须软件包
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
-git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+
 #添加smartdns
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
