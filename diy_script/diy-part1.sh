f33cfbf720144677996fb6898bd8779993827d4d
yaya <tb6750217@gmail.com>
2024-01-20 14:43:35 +0800

    Update diy-part1.sh

--- diy_script/diy-part1.sh
+++ diy_script/diy-part1.sh
@@ -16,4 +16,4 @@
 
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
